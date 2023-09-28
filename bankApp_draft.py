8a2eb3ac36ab309571ca6960b4d47106ecef68bb
Rolf Ahlström <143600486+Roffe122@users.noreply.github.com>
2023-09-28 19:27:33 +0200

    Update Json/debit bankApp_draft.py

--- bankApp_draft.py
+++ bankApp_draft.py
@@ -1,11 +1,11 @@
 # bank app
+import json
+
 class Account:
     def __init__(self, owner, balance, debit, account_id, pin_code):
         self.owner = owner
         self.balance = balance
-        # True om debit, False om kredit
-        # Debit/Kredit används inte i programmet alls, men ni kan utveckla funktioner så att egenskapen är relevant
-        self.debit = bool(debit)
+        self.debit = []
         self.account_id = account_id
         self.pin_code = pin_code
 
@@ -36,6 +36,7 @@ class Account:
             else:
                 self.balance -= amount
                 print(f"{amount} SEK withdrawn.")
+                self.record_debit(amount)
                 self.get_balance()
         except ValueError:
             print("Invalid number.")
@@ -49,56 +50,55 @@ class Account:
                 print("To withdraw the money from the account, use the Withdraw function.")
             else:
                 self.balance += amount
+                self.record_debit(amount)
                 print(f"{amount} SEK deposited.")
                 self.get_balance()
         except ValueError:
             print("Invalid number.")
+
+    def record_debit(self, amount):
+        if amount > 0:
+            self.debit.append(amount)
+
+    def get_debit(self):
+        if self.debit:
+            print(f"Debit History for {self.owner}:")
+            for i, amount in enumerate(self.debit, start=1):
+                print(f"{i}. {amount} SEK")
+        else:
+            print("No debit history available.")
 
     def quit(self):  # Funktionen för att avsluta programmet
         print("=================================")
         print("Thank you for using our services!")
         print("=================================")
 
+def load_accounts():
+    try:
+        with open("accounts.json", "r") as file:
+            data = json.load(file)
 
-# de kan vi ha i en separat json fil och spara
-accounts = [
-    Account("Åsa Åström", 10000, True, 157838, 9885),
-    Account("Elsa Eriksson", 5500, True, 197606, 6214),
-    Account("Olle Olsson", 32000, True, 346709, 2507),
-    Account("Maja Malmström", 7500, True, 231046, 6302),
-    Account("Gustav Gustafsson", 15000, True, 564293, 9149),
-    Account("Sara Svensson", 2800, True, 157507, 9123),
-    Account("Karl Karlsson", 9000, True, 328400, 7465),
-    Account("Emma Engström", 42000, True, 963078, 7619),
-    Account("Nils Nilsson", 6200, True, 404388, 2008),
-    Account("Anna Andersson", 10500, True, 331646, 7345),
-    Account("Per Persson", 2000, True, 221171, 1255),
-    Account("Linnéa Lindberg", 17500, True, 272883, 7328),
-    Account("Anders Åberg", 8800, True, 494224, 5203),
-    Account("Ida Isaksson", 44000, True, 681794, 1164),
-    Account("Erik Eriksson", 6000, True, 994270, 1856),
-    Account("Maria Månsson", 3100, True, 244977, 5595),
-    Account("Johan Johansson", 8700, True, 914766, 4396),
-    Account("Linda Larsson", 12300, True, 684010, 2586),
-    Account("Andreas Andersson", 9300, True, 653698, 2316),
-    Account("Karin Karlsson", 42000, True, 378913, 1989),
-    Account("David Dahlström", 7000, True, 326565, 4576),
-    Account("Anna Andersson", 12500, True, 719667, 8096),
-    Account("Peter Persson", 8800, True, 488449, 3564),
-    Account("Sofia Svensson", 4800, True, 175489, 9081),
-    Account("Jonas Johansson", 11000, True, 739594, 2754),
-    Account("Emma Eriksson", 19000, True, 349790, 6433),
-    Account("Lars Larsson", 7700, True, 227997, 6902),
-    Account("Hanna Holm", 8600, True, 611915, 2978),
-    Account("Mikael Mårtensson", 42000, True, 704174, 6287),
-    Account("Linnea Lindström", 6200, True, 702902, 7469)
-]
+        for account_data in data:
+            if "debit" not in account_data:
+                account_data["debit"] = []
+
+        return [Account(account_data["owner"], account_data["balance"], account_data["debit"], account_data["account_id"], account_data["pin_code"]) for account_data in data]
+    except FileNotFoundError:
+        return []
+
+def save_accounts(accounts):
+    data = [{"owner": account.owner, "balance": account.balance, "debit": account.debit, "account_id": account.account_id, "pin_code": account.pin_code}
+            for account in accounts]
+    with open("accounts.json", "w") as file:
+        json.dump(data, file, indent=5)
+
 
 
 def login_UI():
     print("*" * 20)
     print("* STOCKHOLM BANK *")
     print("*" * 20)
+    accounts = load_accounts()
     user_id = int(input("To log in, enter your id: "))
     pin_code_tries = 3
 
@@ -129,6 +129,7 @@ def main_UI(account):
             print("[c]heck balance")
             print("[w]ithdraw")
             print("[d]eposit")
+            print("[h]istory")
             print("[q]uit")
             action = input(
                 "Select an action: ").lower()  # Metod för att undvika logiska fel vid inmatning av stora bokstäver
@@ -138,6 +139,8 @@ def main_UI(account):
                 account.withdraw()
             elif action == 'd':
                 account.deposit()
+            elif action == 'h':
+                account.get_debit()
             elif action == 'q':
                 account.quit()  # Anropar funktionen för att avsluta programmet
                 break
