8f0d6191dafcd890b91cc053ba9a5180eb3fbe9f
Benjamin Makela <benjamin.makela@yh.nackademin.se>
2023-09-28 23:51:24 +0200

    Satte ihop igors och rolfs versioner och fixade viss kod så att de är kompatibla och fungerar med varandra.

--- bankApp_draft.py
+++ bankApp_draft.py
@@ -1,116 +1,123 @@
 # bank app
+import json
+
+
 class Account:
-    def __init__(self, owner, balance, debit, account_id, pin_code):
+    def __init__(self, owner, account_id, pin_code, balance,
+                 debit):  # debit/Kredit används inte i programmet alls, men ni kan utveckla funktioner så att egenskapen är relevant
         self.owner = owner
-        self.balance = balance
-        # True om debit, False om kredit
-        # Debit/Kredit används inte i programmet alls, men ni kan utveckla funktioner så att egenskapen är relevant
-        self.debit = bool(debit)
         self.account_id = account_id
         self.pin_code = pin_code
+        self.balance = balance
+        self.debit = debit
 
-    # lyckades inte implementera detta ännu, programmet kräver bara account_id att få tillgång till konto
-    def get_access(self):
-        password_input = input("Input your pin-code: (Must be 4 numbers)")
-        try:
-            password_input = int(password_input)
-            if password_input == self.pin_code:
-                print(f"Welcome, {self.owner}")
-            else:
-                print("Account not found.")
-                exit()
-        except ValueError:
-            print("Please enter valid values.")
+    def get_owner(self):  # Igor lade till dessa 3, jag själv vet inte vad de är till men kan ni förklara?
+        return self.owner
+
+    def get_account_id(self):
+        return self.account_id
+
+    def get_pin_code(self):
+        return self.pin_code
 
     def get_balance(self):
         print(f"{self.owner}, your account balance: {self.balance} SEK")
 
     def withdraw(self):
-        amount = input("The sum to withdraw: ")
+        amount = input("Enter the amount to withdraw: ")
         try:
             amount = int(amount)
             if amount < 0:
-                print("Cannot be negative")
+                print("Cannot withdraw a negative amount.")
             elif amount > self.balance:
-                print(f"Cannot withdraw more than {self.balance}")
+                print(f"Cannot withdraw more than your account balance which is: {self.balance} SEK")
             else:
                 self.balance -= amount
                 print(f"{amount} SEK withdrawn.")
                 self.get_balance()
         except ValueError:
-            print("Invalid number.")
+            print("Invalid input. Please enter a valid number.")
 
     def deposit(self):
+        amount = input("Enter the amount to deposit: ")
         print("Please place the money in the machine.")
-        amount = input("How much was put?")
         try:
             amount = int(amount)
             if amount < 0:
-                print("To withdraw the money from the account, use the Withdraw function.")
+                print("Cannot deposit a negative amount on .")
             else:
                 self.balance += amount
-                print(f"{amount} SEK deposited.")
+                print(f"{amount} SEK was successfully deposited in your account.")
                 self.get_balance()
         except ValueError:
-            print("Invalid number.")
+            print("Invalid input. Please enter a valid number.")
+
+    def record_debit(self, amount):
+        if amount > 0:
+            self.debit.append(amount)
+
+    def get_debit(self):
+        if self.debit:
+            print(f"Debit History for {self.owner}:")
+            for i, amount in enumerate(self.debit, start=1):
+                print(f"{i}. {amount} SEK")
+        else:
+            print("No debit history available.")
+
+    def load_accounts():
+        try:
+            with open("accountlist.json", "r") as file:
+                data = json.load(file)
+
+            for account_data in data:
+                if "debit" not in account_data:
+                    account_data["debit"] = []
+
+            return [Account(account_data["owner"], account_data["account_id"], account_data["pin_code"],
+                            account_data["balance"], account_data["debit"]) for account_data in data]
+        except FileNotFoundError:
+            return []
+
+    def save_accounts(accounts):
+        data = [{"owner": account.owner, "account_id": account.account_id, "pin_code": account.pin_code,
+                 "balance": account.balance, "debit": account.debit}
+                for account in accounts]
+        with open("accountlist.json", "w") as file:
+            json.dump(data, file, indent=5)
+
+    def login_UI():
+        print("*" * 20)
+        print("* STOCKHOLM BANK *")
+        print("*" * 20)
+        accounts = Account.load_accounts()
+        user_id = int(input("To log in, enter your id: "))
+        pin_code_tries = 3
+
+        for account in accounts:
+            if user_id == int(account.account_id):
+                while pin_code_tries > 0:
+                    user_pincode = input("Enter your pin code: ")
+                    for account in accounts:
+                        if user_pincode == str(account.pin_code):
+                            return account
+                    else:  # fel pin code, avslutar programmet nu men kan utvecklas mer så att kontot låses t.ex.
+                        pin_code_tries -= 1
+                        if pin_code_tries == 0:
+                            print("You have entered the wrong pin code too many times. Your account has been locked.")
+                            return Account.login_UI()
+                        print(f"Pin code incorrect. You have {pin_code_tries} tries left.")
+                        continue
+        else:
+            print("*" * 20)
+            print("Account not found.")  # Konto-ID hittades inte
+            print("*" * 20)
+            print("Try again.")
+            return Account.login_UI()  # fortsätt med begäran och ange ID
 
     def quit(self):  # Funktionen för att avsluta programmet
         print("=================================")
         print("Thank you for using our services!")
         print("=================================")
-
-
-# de kan vi ha i en separat json fil och spara
-accounts = [
-    Account("Åsa Åström", 10000, True, 121212, 1212),
-    Account("Elsa Eriksson", 5500, True, 234567, 3456),
-    Account("Olle Olsson", 32000, True, 987654, 7890),
-    Account("Maja Malmström", 7500, True, 567890, 1234),
-    Account("Gustav Gustafsson", 15000, True, 345678, 5678),
-    Account("Sara Svensson", 2800, True, 456789, 2345),
-    Account("Karl Karlsson", 9000, True, 789012, 6789),
-    Account("Emma Engström", 42000, True, 345678, 1234),
-    Account("Nils Nilsson", 6200, True, 123456, 3456),
-    Account("Anna Andersson", 10500, True, 567890, 7890),
-    Account("Per Persson", 2000, True, 987654, 2345),
-    Account("Linnéa Lindberg", 17500, True, 234567, 5678),
-    Account("Anders Åberg", 8800, True, 345678, 1234),
-    Account("Ida Isaksson", 44000, True, 121212, 3456),
-    Account("Erik Eriksson", 6000, True, 789012, 5678),
-    Account("Maria Månsson", 3100, True, 345678, 7890),
-    Account("Johan Johansson", 8700, True, 567890, 2345),
-    Account("Linda Larsson", 12300, True, 987654, 1234),
-    Account("Andreas Andersson", 9300, True, 567890, 5678),
-    Account("Karin Karlsson", 42000, True, 345678, 2345),
-    Account("David Dahlström", 7000, True, 123456, 1234),
-    Account("Anna Andersson", 12500, True, 567890, 3456),
-    Account("Peter Persson", 8800, True, 345678, 2345),
-    Account("Sofia Svensson", 4800, True, 234567, 5678),
-    Account("Jonas Johansson", 11000, True, 345678, 1234),
-    Account("Emma Eriksson", 19000, True, 987654, 7890),
-    Account("Lars Larsson", 7700, True, 567890, 3456),
-    Account("Hanna Holm", 8600, True, 234567, 1234),
-    Account("Mikael Mårtensson", 42000, True, 345678, 5678),
-    Account("Linnea Lindström", 6200, True, 123456, 2345)
-]
-
-
-def login_UI():
-    print("*" * 20)
-    print("* STOCKHOLM BANK *")
-    print("*" * 20)
-    user_id = input("To log in, enter your id: ")
-    for account in accounts:
-        if user_id == str(account.account_id):
-            user_pincode = input("Enter your pin code: ")
-            for account in accounts:
-                if user_pincode == str(account.pin_code):
-                    return account
-                elif user_pincode != str(account.pin_code):  # fel pin code
-                    return print("Pin code incorrect.")
-        else:
-            return print("Account not found.")  # Konto-ID hittades inte
-
 
 
 def main_UI(account):
@@ -122,6 +129,7 @@ def main_UI(account):
             print("[c]heck balance")
             print("[w]ithdraw")
             print("[d]eposit")
+            print("[h]istory")
             print("[q]uit")
             action = input(
                 "Select an action: ").lower()  # Metod för att undvika logiska fel vid inmatning av stora bokstäver
@@ -131,6 +139,8 @@ def main_UI(account):
                 account.withdraw()
             elif action == 'd':
                 account.deposit()
+            elif action == 'h':
+                account.get_debit()
             elif action == 'q':
                 account.quit()  # Anropar funktionen för att avsluta programmet
                 break
@@ -138,6 +148,6 @@ def main_UI(account):
                 print("Invalid action. Please select a valid option.")
 
 
-logged_in_account = login_UI()
+logged_in_account = Account.login_UI()
 if logged_in_account:
     main_UI(logged_in_account)
